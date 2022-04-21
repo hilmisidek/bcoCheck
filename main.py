8a445646dea32a0bada235f390c2a3db9203cfcc
hilmisidek <zalikamari@gmail.com>
2022-04-21 12:25:07 +0800

    2nd

--- main.py
+++ main.py
@@ -123,7 +123,7 @@ def main():
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
                           url_path=TOKEN)
-    updater.bot.setWebhook('powerful-ravine-83013.herokuapp.com/' + TOKEN)
+    updater.bot.setWebhook('bcocheck.herokuapp.com/' + TOKEN)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
